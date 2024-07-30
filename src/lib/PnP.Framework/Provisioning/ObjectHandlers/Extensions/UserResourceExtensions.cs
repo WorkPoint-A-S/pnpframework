f8a7bbe7b7092d7a374522d583d026aa92a6d451
Bjørn Henneberg Christiansen <bhc@workpoint.dk>
2024-07-30 11:33:42 +0200

    added unique identifier to temporary file name added try finally to ensure deletion of file

--- src/lib/PnP.Framework/Provisioning/ObjectHandlers/Extensions/UserResourceExtensions.cs
+++ src/lib/PnP.Framework/Provisioning/ObjectHandlers/Extensions/UserResourceExtensions.cs
@@ -19,52 +19,61 @@ namespace PnP.Framework.Provisioning.ObjectHandlers.Extensions
         {
             var tempFolder = Path.GetTempPath();
             List<Tuple<string, int, string>> resourceTokens = creationInfo.ResourceTokens;
-
             var languages = resourceTokens.Select(t => t.Item2).Distinct();
             foreach (int language in languages)
             {
                 var culture = new CultureInfo(language);
+                // Use a unique file name by appending a GUID
+                var uniqueFileName = $"{creationInfo.ResourceFilePrefix}.{culture.Name}.{Guid.NewGuid()}.resx";
+                var resourceFileName = Path.Combine(tempFolder, uniqueFileName);
+                var resourceFileToDelete = resourceFileName;
 
-                var resourceFileName = Path.Combine(tempFolder, $"{creationInfo.ResourceFilePrefix}.{culture.Name}.resx");
-                if (System.IO.File.Exists(resourceFileName))
+                try
                 {
-                    // Read existing entries, if any
-                    using (ResourceReader resxReader = new ResourceReader(resourceFileName))
-                    //using (ResXResourceReader resxReader = new ResXResourceReader(resourceFileName))
+                    if (System.IO.File.Exists(resourceFileName))
                     {
-                        foreach (DictionaryEntry entry in resxReader)
+                        // Read existing entries, if any
+                        using (ResourceReader resxReader = new ResourceReader(resourceFileName))
                         {
-                            // find if token is already there
-                            var existingToken = resourceTokens.FirstOrDefault(t => t.Item1 == entry.Key.ToString() && t.Item2 == language);
-                            if (existingToken == null)
+                            foreach (DictionaryEntry entry in resxReader)
                             {
-                                resourceTokens.Add(new Tuple<string, int, string>(entry.Key.ToString(), language, entry.Value as string));
+                                // find if token is already there
+                                var existingToken = resourceTokens.FirstOrDefault(t => t.Item1 == entry.Key.ToString() && t.Item2 == language);
+                                if (existingToken == null)
+                                {
+                                    resourceTokens.Add(new Tuple<string, int, string>(entry.Key.ToString(), language, entry.Value as string));
+                                }
                             }
                         }
                     }
-                }
 
-                // Create new resource file - use implementation copied from .Net Framework cause otherwise we end up with binary serialized files
-                //using (ResourceWriter resx = new ResourceWriter(resourceFileName))
-                using (ResXResourceWriter resx = new ResXResourceWriter(resourceFileName))
-                {
-                    foreach (var token in resourceTokens.Where(t => t.Item2 == language))
+                    // Create new resource file - use implementation copied from .Net Framework cause otherwise we end up with binary serialized files
+                    using (ResXResourceWriter resx = new ResXResourceWriter(resourceFileName))
                     {
+                        foreach (var token in resourceTokens.Where(t => t.Item2 == language))
+                        {
+                            resx.AddResource(token.Item1, token.Item3);
+                        }
+                    }
 
-                        resx.AddResource(token.Item1, token.Item3);
+                    template.Localizations.Add(new Localization() { LCID = language, Name = culture.NativeName, ResourceFile = uniqueFileName });
+
+                    // Persist the file using the connector
+                    using (FileStream stream = System.IO.File.Open(resourceFileName, FileMode.Open))
+                    {
+                        creationInfo.FileConnector.SaveFileStream(uniqueFileName, stream);
                     }
                 }
-
-                template.Localizations.Add(new Localization() { LCID = language, Name = culture.NativeName, ResourceFile = $"{creationInfo.ResourceFilePrefix}.{culture.Name}.resx" });
-
-                // Persist the file using the connector
-                using (FileStream stream = System.IO.File.Open(resourceFileName, FileMode.Open))
+                finally
                 {
-                    creationInfo.FileConnector.SaveFileStream($"{creationInfo.ResourceFilePrefix}.{culture.Name}.resx", stream);
+                    // Ensure the temp resx file is deleted even if an exception occurs
+                    if (System.IO.File.Exists(resourceFileToDelete))
+                    {
+                        System.IO.File.Delete(resourceFileToDelete);
+                    }
                 }
-                // remove the temp resx file
-                System.IO.File.Delete(resourceFileName);
             }
+
             return template;
         }
 
@@ -87,17 +96,17 @@ namespace PnP.Framework.Provisioning.ObjectHandlers.Extensions
 
         public static bool PersistResourceValue(UserResource userResource, string token, ProvisioningTemplate template, ProvisioningTemplateCreationInformation creationInfo, bool skipWebLanguage)
         {
-			(userResource.Context as ClientContext).Web.EnsureProperty(w => w.Language);
+            (userResource.Context as ClientContext).Web.EnsureProperty(w => w.Language);
 
-			bool returnValue = false;
+            bool returnValue = false;
             List<Tuple<string, int, string>> resourceTokens = creationInfo.ResourceTokens;
 
             foreach (var language in template.SupportedUILanguages)
             {
-				if (skipWebLanguage && language.LCID == (userResource.Context as ClientContext).Web.Language) //Ignore default language
-					continue;
+                if (skipWebLanguage && language.LCID == (userResource.Context as ClientContext).Web.Language) //Ignore default language
+                    continue;
 
-				var culture = new CultureInfo(language.LCID);
+                var culture = new CultureInfo(language.LCID);
 
                 var value = userResource.GetValueForUICulture(culture.Name);
                 userResource.Context.ExecuteQueryRetry();
@@ -110,7 +119,7 @@ namespace PnP.Framework.Provisioning.ObjectHandlers.Extensions
             }
 
             return returnValue;
-		}
+        }
 
         public static bool PersistResourceValue(string token, int lcid, string title, ProvisioningTemplateCreationInformation creationInfo)
         {
